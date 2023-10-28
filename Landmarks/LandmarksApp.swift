152535aef4056e62b2b08e8c94b29f1fd0f5d0eb
enoch <enirox001@gmail.com>
2023-10-28 08:27:19 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ehnamuram Enoch on 28/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
